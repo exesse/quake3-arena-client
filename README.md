c2661fd8d27cb141fb7a6c4d44091e7f45a94a95
exesse <kulbatski@gmail.com>
2020-12-26 18:59:28 +0100

    quake-2-client: release

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # quake3-arena-client
-Quake III Arena installer for Ubuntu/Debian
+Quake III Arena installer for Ubuntu/Debian.
+
+This is a guide for Quake III Arena on Ubuntu/Debian.
+
+You will need an original **pk0.pk3** file from the game CD or [digital](https://github.com/nrempel/q3-server/raw/master/baseq3/pak0.pk3) [download](http://game.pioneernet.ru/dl/q3/files/pk3/pak0.pk3).
+
+## Usage 
+```bash
+git git@github.com:exesse/quake3-arena-client.git
+./quake3-arena-client/install.sh {Path or URL to pk0.pk3 original file}
+```
+
+##
+Email bug reports, questions, discussions to [hi@exesse.org](mailto:hi@exesse.org).
---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+QUAKE_INSTALL_PATH=~/.q3a/baseq3
+QUAKE_PAK=~/.q3a/baseq3/pak0.pk3
+VAILD_CHECKSUM=7ce8b3910620cd50a09e4f1100f426e8c6180f68895d589f80e6bd95af54bcae
+
+
+if [[ "$*" == "" ]]; then
+    echo "ERROR - no arguments provided."
+    echo ""
+    echo "Quake III Arena Client Installer."
+    echo ""
+    echo "Usage: ./install.sh {path to pak0.pk3 file on local system | url to pak0.pk3 file that will be downloaded} [OPTION]"
+    echo ""
+    echo "Options:"
+    echo "--no-check-sum - ignore sha256 checksum check of the pak0.pk3 file"
+    echo ""
+    echo "Help:"
+    echo "-h, --help     - display this message"
+    echo ""
+    echo "Email bug reports, questions, discussions to <hi@exesse.org>"
+    exit 1
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    echo "Quake III Arena Client Installer."
+    echo ""
+    echo "Usage: ./install.sh {path to pak0.pk3 file on local system | url to pak0.pk3 file that will be downloaded} [OPTION]"
+    echo ""
+    echo "Options:"
+    echo "--no-check-sum - ignore sha256 checksum check of the pak0.pk3 file"
+    echo ""
+    echo "Help:"
+    echo "-h, --help     - display this message"
+    echo ""
+    echo "Email bug reports, questions, discussions to <hi@exesse.org>"
+    exit 1
+else
+    QUAKE_PAK_ARCHIVE=$1
+    sudo apt update
+    sudo apt install wget quake3 quake3-data game-data-packager innoextract
+    mkdir -p ${QUAKE_INSTALL_PATH}
+    if [[ -f "$QUAKE_PAK_ARCHIVE" ]]; then
+        if [[ "$2" == "" ]];then
+            LOADED_FILE_HASH=($(sha256sum ${QUAKE_PAK_ARCHIVE}))
+            if [[ "$LOADED_FILE_HASH" != "$VAILD_CHECKSUM" ]]; then
+                echo "ERROR - checksums does not match."
+                exit 1
+            fi
+        fi
+        cp ${QUAKE_PAK_ARCHIVE} ${QUAKE_PAK}
+    else
+        wget -O ${QUAKE_PAK} ${QUAKE_PAK_ARCHIVE}
+            if [[ -f "$QUAKE_PAK" ]]; then
+                if [[ "$2" == "" ]];then
+                    LOADED_FILE_HASH=($(sha256sum ${QUAKE_PAK}))
+                        if [[ "$LOADED_FILE_HASH" != "$VAILD_CHECKSUM" ]]; then
+                            echo "ERROR - checksums does not match."
+                            exit 1
+                        fi
+                fi
+                echo "$QUAKE_PAK was successfully downloaded."
+            else
+                echo "Was not able to get Quake III source package."
+                exit 1
+            fi
+    fi
+    game-data-packager -i quake3 ${QUAKE_PAK}
+fi
